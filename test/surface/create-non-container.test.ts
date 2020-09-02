30aca5d99981d95de4acdf23aa24789f640a5147
Michiel de Jong <michiel@unhosted.org>
2020-09-02 16:08:16 +0200

    mkdir-p tests

--- test/surface/create-non-container.test.ts
+++ test/surface/create-non-container.test.ts
@@ -188,7 +188,7 @@ describe('Create non-container', () => {
       afterAll(() => recursiveDelete(location, authFetcher));
     });
 
-    describe.only('using PATCH', () => {
+    describe('using PATCH', () => {
       const { testFolderUrl } = generateTestFolder();
       let websocketsPubsubClientContainer;
       let websocketsPubsubClientResource;
@@ -251,6 +251,200 @@ describe('Create non-container', () => {
       });
       afterAll(() => recursiveDelete(location, authFetcher));
     });
+  });
 
+  describe('in an non-existing container', () => {
+    describe('using PUT with If-None-Match header', () => {
+      const { testFolderUrl } = generateTestFolder();
+      const containerUrl = `${testFolderUrl}new/`;
+      let websocketsPubsubClientParent;
+      let websocketsPubsubClientContainer;
+      let websocketsPubsubClientResource;
+      const resourceUrl = `${containerUrl}new.ttl`;
+
+      beforeAll(async () => {
+        websocketsPubsubClientParent = new WPSClient(testFolderUrl, authFetcher);
+        await websocketsPubsubClientParent.getReady();
+        websocketsPubsubClientContainer = new WPSClient(containerUrl, authFetcher);
+        await websocketsPubsubClientContainer.getReady();
+        websocketsPubsubClientResource = new WPSClient(resourceUrl, authFetcher);
+        await websocketsPubsubClientResource.getReady();
+        const result = await authFetcher.fetch(resourceUrl, {
+          method: 'PUT',
+          header: {
+            'Content-Type': 'text/plain',
+            'If-None-Match': '*'
+          },
+          body: 'Hello World'
+        });
+      });
+
+      afterAll(() => {
+        websocketsPubsubClientParent.disconnect();
+        websocketsPubsubClientContainer.disconnect();
+        websocketsPubsubClientResource.disconnect();
+        recursiveDelete(testFolderUrl, authFetcher);
+      });
+
+      it('creates the resource', async () => {
+        const result = await authFetcher.fetch(resourceUrl);
+        expect(responseCodeGroup(result.status)).toEqual('2xx');
+        expect(await result.text()).toEqual('Hello World');
+        expect(result.headers.get('Content-Type')).toEqual('text/plain');
+        
+      });
+      it('adds the resource in the container listing', async () => {
+        const containerListing = await getContainerMembers(containerUrl, authFetcher);
+        expect(containerListing.sort()).toEqual([
+          `${containerUrl}exists.ttl`,
+          resourceUrl
+        ].sort());
+      });
+      it('emits websockets-pubsub on the parent', () => {
+        expect(websocketsPubsubClientParent.received).toEqual([
+          `ack ${testFolderUrl}`,
+          `pub ${testFolderUrl}`
+        ]);
+      });
+      it('emits websockets-pubsub on the container', () => {
+        expect(websocketsPubsubClientContainer.received).toEqual([
+          `ack ${containerUrl}`,
+          `pub ${containerUrl}`
+        ]);
+      });
+      it('emits websockets-pubsub on the resource', () => {
+        expect(websocketsPubsubClientResource.received).toEqual([
+          `ack ${resourceUrl}`,
+          `pub ${resourceUrl}`
+        ]);
+      });
+      afterAll(() => recursiveDelete(location, authFetcher));
+    });
+  
+    describe('using PUT without If-None-Match header', () => {
+      const { testFolderUrl } = generateTestFolder();
+      let websocketsPubsubClientParent;
+      let websocketsPubsubClientContainer;
+      let websocketsPubsubClientResource;
+      const containerUrl = `${testFolderUrl}new/`;
+      const resourceUrl = `${containerUrl}new.ttl`;
+
+      beforeAll(async () => {
+        websocketsPubsubClientParent = new WPSClient(testFolderUrl, authFetcher);
+        await websocketsPubsubClientParent.getReady();
+        websocketsPubsubClientContainer = new WPSClient(containerUrl, authFetcher);
+        await websocketsPubsubClientContainer.getReady();
+        websocketsPubsubClientResource = new WPSClient(resourceUrl, authFetcher);
+        await websocketsPubsubClientResource.getReady();
+        const result = await authFetcher.fetch(resourceUrl, {
+          method: 'PUT',
+          header: {
+            'Content-Type': 'text/plain'
+          },
+          body: 'Hello World'
+        });
+      });
+
+      afterAll(() => {
+        websocketsPubsubClientParent.disconnect();
+        websocketsPubsubClientContainer.disconnect();
+        websocketsPubsubClientResource.disconnect();
+        recursiveDelete(testFolderUrl, authFetcher);
+      });
+
+      it('creates the resource', async () => {
+        const result = await authFetcher.fetch(resourceUrl);
+        expect(result.status).toEqual(404);        
+      });
+      it('does not add the resource in the container listing', async () => {
+        const containerListing = await getContainerMembers(containerUrl, authFetcher);
+        expect(containerListing.sort()).toEqual([
+          `${containerUrl}exists.ttl`
+        ].sort());
+      });
+      it('Does not emit websockets-pubsub on the parent', () => {
+        expect(websocketsPubsubClientParent.received).toEqual([
+          `ack ${testFolderUrl}`
+        ]);
+      });
+      it('Does not emit websockets-pubsub on the container', () => {
+        expect(websocketsPubsubClientContainer.received).toEqual([
+          `ack ${containerUrl}`
+        ]);
+      });
+      it('Does not emit websockets-pubsub on the resource', () => {
+        expect(websocketsPubsubClientResource.received).toEqual([
+          `ack ${resourceUrl}`
+        ]);
+      });
+      afterAll(() => recursiveDelete(location, authFetcher));
+    });
+
+    describe('using PATCH', () => {
+      const { testFolderUrl } = generateTestFolder();
+      let websocketsPubsubClientParent;
+      let websocketsPubsubClientContainer;
+      let websocketsPubsubClientResource;
+      const containerUrl = `${testFolderUrl}new/`;
+      const resourceUrl = `${containerUrl}new.ttl`;
+
+      beforeAll(async () => {
+        websocketsPubsubClientParent = new WPSClient(testFolderUrl, authFetcher);
+        await websocketsPubsubClientParent.getReady();
+        websocketsPubsubClientContainer = new WPSClient(containerUrl, authFetcher);
+        await websocketsPubsubClientContainer.getReady();
+        websocketsPubsubClientResource = new WPSClient(resourceUrl, authFetcher);
+        await websocketsPubsubClientResource.getReady();
+        const result = await authFetcher.fetch(resourceUrl, {
+          method: 'PATCH',
+          body: 'INSERT DATA { <#hello> <#linked> <#world> . }',
+          headers: {
+            'Content-Type': 'application/sparql-update'
+          }
+        });
+      });
+
+      afterAll(() => {
+        websocketsPubsubClientParent.disconnect();
+        websocketsPubsubClientContainer.disconnect();
+        websocketsPubsubClientResource.disconnect();
+        recursiveDelete(testFolderUrl, authFetcher);
+      });
+
+      it('creates the resource', async () => {
+        const result = await authFetcher.fetch(resourceUrl);
+        expect(responseCodeGroup(result.status)).toEqual('2xx');
+        // FIXME: Check only RDF content here, not precise Turtle syntax:
+        expect(await result.text()).toEqual('@prefix : <#>.\n\n:hello :linked :world.\n\n');
+        expect(result.headers.get('Content-Type')).toEqual('text/turtle');
+        
+      });
+      it('adds the resource in the container listing', async () => {
+        const containerListing = await getContainerMembers(containerUrl, authFetcher);
+        expect(containerListing.sort()).toEqual([
+          `${containerUrl}exists.ttl`,
+          resourceUrl
+        ].sort());
+      });
+      it('emits websockets-pubsub on the parent', () => {
+        expect(websocketsPubsubClientParent.received).toEqual([
+          `ack ${testFolderUrl}`,
+          `pub ${testFolderUrl}`
+        ]);
+      });
+      it('emits websockets-pubsub on the container', () => {
+        expect(websocketsPubsubClientContainer.received).toEqual([
+          `ack ${containerUrl}`,
+          `pub ${containerUrl}`
+        ]);
+      });
+      it('emits websockets-pubsub on the resource', () => {
+        expect(websocketsPubsubClientResource.received).toEqual([
+          `ack ${resourceUrl}`,
+          `pub ${resourceUrl}`
+        ]);
+      });
+      afterAll(() => recursiveDelete(location, authFetcher));
+    });
   });
 });
