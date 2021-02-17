7afaf8db780c8616ec8727c8b195b30ee837fecb
Michiel de Jong <michiel@unhosted.org>
2021-02-17 06:08:08 +0100

    PUT to overwrite, plain->plain and turtle->turtle

--- test/surface/update.test.ts
+++ test/surface/update.test.ts
@@ -38,7 +38,60 @@ describe("Update", () => {
     }
     authFetcher = await getAuthFetcher(oidcIssuer, newCookie, appOrigin);
   });
-  describe("Using PUT (same content type)", () => {
+  describe("Using PUT, overwriting plain text with plain text", () => {
+    const { testFolderUrl } = generateTestFolder();
+    let websocketsPubsubClientResource;
+    const containerUrl = `${testFolderUrl}exists/`;
+    const resourceUrl = `${containerUrl}exists1.txt`;
+
+    beforeAll(async () => {
+      // this already relies on the PUT to non-existing folder functionality
+      // that will be one of the tested behaviours:
+      await authFetcher.fetch(resourceUrl, {
+        method: "PUT",
+        body: "Hello World",
+        headers: {
+          "Content-Type": "text/plain",
+        },
+      });
+      await new Promise((resolve) => setTimeout(resolve, waittime));
+      const getResult = await authFetcher.fetch(resourceUrl);
+      const resourceETagInQuotes = getResult.headers.get("ETag");
+      websocketsPubsubClientResource = new WPSClient(resourceUrl, authFetcher);
+      await websocketsPubsubClientResource.getReady();
+      const headers = {
+        "Content-Type": "text/plain"
+      };
+      if (resourceETagInQuotes) {
+        headers["If-Match"] = resourceETagInQuotes;
+      }
+      const result = await authFetcher.fetch(resourceUrl, {
+        method: "PUT",
+        headers,
+        body: "Replaced the contents.",
+      });
+      await new Promise((resolve) => setTimeout(resolve, waittime));
+    });
+
+    afterAll(() => {
+      websocketsPubsubClientResource.disconnect();
+      recursiveDelete(testFolderUrl, authFetcher);
+    });
+
+    it("updates the resource", async () => {
+      const result = await authFetcher.fetch(resourceUrl);
+      expect(responseCodeGroup(result.status)).toEqual("2xx");
+      expect(await result.text()).toEqual("Replaced the contents.");
+      expect(result.headers.get("Content-Type")).toContain("text/plain");
+    });
+    ifWps("emits websockets-pubsub on the resource", () => {
+      expect(websocketsPubsubClientResource.received).toEqual(
+        expect.arrayContaining([`ack ${resourceUrl}`, `pub ${resourceUrl}`])
+      );
+    });
+  });
+
+  describe("Using PUT, overwriting Turtle with Turtle", () => {
     const { testFolderUrl } = generateTestFolder();
     let websocketsPubsubClientResource;
     const containerUrl = `${testFolderUrl}exists/`;
@@ -90,7 +143,9 @@ describe("Update", () => {
       );
       rdflib.parse(await result.text(), store2, resourceUrl, "text/turtle");
 
-      expect(store2.toString()).toEqual(store1.toString());
+      expect(store2.statements).toEqual(
+        expect.arrayContaining(store1.statements)
+      );
       expect(result.headers.get("Content-Type")).toContain("text/turtle");
     });
     ifWps("emits websockets-pubsub on the resource", () => {
@@ -100,7 +155,7 @@ describe("Update", () => {
     });
   });
 
-  describe("Using PUT (same content)", () => {
+  describe("Using PUT (same Turtle content)", () => {
     const { testFolderUrl } = generateTestFolder();
     let websocketsPubsubClientResource;
     const containerUrl = `${testFolderUrl}exists/`;
@@ -328,10 +383,7 @@ describe("Update", () => {
     });
     ifWps("emits websockets-pubsub on the resource", () => {
       expect(websocketsPubsubClientResource.received).toEqual(
-        expect.arrayContaining([
-          `ack ${resourceUrl}`,
-          `pub ${resourceUrl}`
-        ])
+        expect.arrayContaining([`ack ${resourceUrl}`, `pub ${resourceUrl}`])
       );
     });
   });
@@ -392,10 +444,7 @@ describe("Update", () => {
     });
     ifWps("emits websockets-pubsub on the resource", () => {
       expect(websocketsPubsubClientResource.received).toEqual(
-        expect.arrayContaining([
-          `ack ${resourceUrl}`,
-          `pub ${resourceUrl}`
-        ])
+        expect.arrayContaining([`ack ${resourceUrl}`, `pub ${resourceUrl}`])
       );
     });
   });
