2c420f47f1c98f603caab51d9de1cc411bd9388b
Michiel de Jong <michiel@unhosted.org>
2021-03-04 19:37:40 +0100

    run-against-trinpod.sh

---
+++ run-against-trinpod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# npm ci
+
+export CURL_RESULT=`curl -i https://solidcommunity.net/login/password -d"username=solidtestsuite&password=Testing123" | grep Set-Cookie`
+export COOKIE=`expr "$CURL_RESULT" : '^Set-Cookie:\ \(.*\).'`
+echo $COOKIE
+export SERVER_ROOT=https://solidcommunity.net
+export STORAGE_ROOT=https://solidtestsuite.trinpod.us/solidtestsuite
+export ALICE_WEBID=https://solidtestsuite.solidcommunity.net/profile/card#me
+export SKIP_WPS=1
+export DEBUG=*
+env
+./node_modules/.bin/jest test/surface/
