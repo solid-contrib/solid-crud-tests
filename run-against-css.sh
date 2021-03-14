a67f10a31e8aa470d687b08f49ee9086cd622a2f
bourgeoa <alain.bourgeois10@gmail.com>
2021-03-14 18:32:14 +0100

    add mkdir

--- run-against-css.sh
+++ run-against-css.sh
@@ -8,5 +8,8 @@ export ALICE_WEBID_DOC=${SERVER_ROOT}/profile.ttl
 export ALICE_WEBID=$ALICE_WEBID_DOC#me
 curl -X PUT $ALICE_WEBID_DOC -H 'Content-Type: text/turtle' -d "<$ALICE_WEBID> <http://www.w3.org/ns/pim/space#storage> </>."
 
+mkdir -p ../test-suite/CSS
 export INCLUDE_MAY=1
-./node_modules/.bin/jest test/surface/ --json --outputFile="../test-suite/CSS/crud-results.json"
+mkdir -p ../test-suite/CSS
+./node_modules/.bin/jest test/surface/ --verbose --json --outputFile="../test-suite/CSS/crud-results.json"
+
