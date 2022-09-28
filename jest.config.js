cd04431d3c281dccf14994aed25d86872ba7de80
Michiel de Jong <michiel@unhosted.org>
2022-09-28 16:59:47 +0200

    work around https://github.com/gabrieli/jest-serial-runner/issues/16

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   testEnvironment: "node",
   testTimeout: 60000,
   verbose: false,
-  runner: "jest-serial-runner",
+  runner: "@codejedi365/jest-serial-runner",
   collectCoverage: false,
   setupFilesAfterEnv: ["jest-extended"],
   globals: {
