c6f02d8351bb9432040d9b38140442968d5c8025
Michiel de Jong <michiel@unhosted.org>
2022-09-30 13:55:40 +0200

    skeleton of NotificationsClient with 3 protocols

--- test/helpers/NotificationsClient.ts
+++ test/helpers/NotificationsClient.ts
@@ -2,8 +2,22 @@ import { getAuthHeaders } from "solid-auth-fetcher";
 import AuthFetcher from "solid-auth-fetcher/dist/AuthFetcher";
 import WebSocket = require("ws");
 import parseLinkHeader = require("parse-link-header");
+import { URL } from "url";
 
 const PROTOCOL_STRING = "solid-0.1";
+
+function tryRel(obj: any, rel: string, base: string): string | undefined {
+  // console.log(obj);
+  if (obj[rel] === undefined) {
+    return;
+  }
+  const urlStr = obj[rel]["url"];
+  console.log("relative", urlStr);
+  if (typeof urlStr === "string") {
+    const urlObj = new URL(urlStr, base);
+    return urlObj.toString();
+  }
+}
 
 export class NotificationsClient {
   received: string[];
@@ -19,14 +33,20 @@ export class NotificationsClient {
     this.authFetcher = authFetcher;
     this.disabled = !!process.env.SKIP_WPS;
   }
-  async getReady(): Promise<void> {
-    if (this.disabled) {
-      return;
-    }
-    const result = await this.authFetcher.fetch(this.resourceUrl, {
+  async getLinksToNotifications(): Promise<{
+    insecureWs?: string;
+    storageWide?: string;
+    resourceSpecific?: string;
+  }> {
+    const result = {
+      insecureWs: undefined,
+      storageWide: undefined,
+      resourceSpecific: undefined,
+    };
+    const resourceFetchResult = await this.authFetcher.fetch(this.resourceUrl, {
       method: "HEAD",
     });
-    const linkHeaders = result.headers.raw()["link"];
+    const linkHeaders = resourceFetchResult.headers.raw()["link"];
     if (Array.isArray(linkHeaders) && linkHeaders.length > 0) {
       let obj = {};
       for (let i = 0; i < linkHeaders.length; i++) {
@@ -35,17 +55,79 @@ export class NotificationsClient {
           ...obj,
         };
       }
-      console.log(obj);
+      if (!result.storageWide) {
+        result.storageWide = tryRel(
+          obj,
+          "http://www.w3.org/ns/solid#storageDescription",
+          this.resourceUrl
+        );
+      }
+      if (!result.resourceSpecific) {
+        result.resourceSpecific = tryRel(obj, "describedby", this.resourceUrl);
+      }
     }
-    const serverWideNotificationsDescription = obj["http://www.w3.org/ns/solid#storageDescription"];
 
+    result.insecureWs = resourceFetchResult.headers.get("updates-via");
+    return result;
+  }
+  async fetchAndParseDescription(url: string) {
+    // console.log("absolute", serverWideNotificationsDescription);
+    const descriptionFetchResult = await this.authFetcher.fetch(url, {
+      headers: {
+        Accept: "application/ld_json",
+      },
+    });
+    const obj = await descriptionFetchResult.json();
+    return obj.notificationChannel;
     /// tbc!
-    
-    const wssUrl = result.headers.get("updates-via");
-    if (wssUrl.length > 0) {
-      this.setupInsecureWs(wssUrl);
+  }
+
+  async subscribeToChannels(descriptionUrl: string): Promise<void> {
+    // console.log("subscribing to channels", descriptionUrl);
+    const channels = await this.fetchAndParseDescription(descriptionUrl);
+    for (let i = 0; i < channels.length; i++) {
+      // console.log(channels[i]);
+      if (channels[i].type == "WebSocketNotifications2021") {
+        this.setupSecureWs(channels[i].subscription);
+      }
+      if (channels[i].type == "WebHookNotifications2022") {
+        this.setupWebHookClient(channels[i].subscription);
+      }
     }
   }
+
+  async getReady(): Promise<void> {
+    if (this.disabled) {
+      return;
+    }
+    const descriptions = await this.getLinksToNotifications();
+    if (
+      typeof descriptions.insecureWs === "string" &&
+      descriptions.insecureWs.length > 0
+    ) {
+      this.setupInsecureWs(descriptions.insecureWs);
+    }
+    if (
+      typeof descriptions.storageWide === "string" &&
+      descriptions.storageWide.length > 0
+    ) {
+      this.subscribeToChannels(descriptions.storageWide);
+    }
+    if (
+      typeof descriptions.resourceSpecific === "string" &&
+      descriptions.resourceSpecific.length > 0
+    ) {
+      this.subscribeToChannels(descriptions.resourceSpecific);
+    }
+  }
+
+  async setupSecureWs(wssUrl: string): Promise<void> {
+    // TODO: implement
+  }
+  async setupWebHookClient(subscribeUrl: string): Promise<void> {
+    // TODO: implement
+  }
+
   async setupInsecureWs(wssUrl: string): Promise<void> {
     this.insecureWs = new WebSocket(wssUrl, PROTOCOL_STRING);
     this.insecureWs.on("message", (msg) => {
